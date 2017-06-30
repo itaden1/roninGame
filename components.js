c34c6534be15dd3bc4cbf1fd04ee9de060ad4db6
Ethan Shearer <ethanshearer1@gmail.com>
2017-06-30 15:19:25 +1000

    cleanup, rework level map, seperated components

---
+++ components.js
@@ -0,0 +1,146 @@
+
+//constructor for player object
+function component(width, height, color, x, y, name,img){
+	
+	//setbasic values
+	this.name = name;
+	this.col = color;
+
+	//map position of character
+	this.x = x;
+	this.y = y;
+
+	//set size
+	this.width = width;
+	this.height = height;
+	
+	//image to be used, image size and image location in relation to object location 
+	this.img = img;
+	this.imgX = this.x-this.width/4;
+	this.imgY = this.y;
+	this.imgSrcX = 0;
+	this.imgSrcY = 0;
+	this.imgWidth = width*2;
+	this.imgHeight = height;
+	
+	//physics
+	this.canJump = true;
+	this.velocityX = 0;
+	this.velocityY = 0;
+	this.maxSpeed = 5;
+	this.gravity = 0.3;
+	this.friction = 0.8;
+	this.acceleration = 1.5;
+
+	//bounding boxes for collision detection, color value is for testing purposes
+	this.bbu = {col:'yellow',x:this.x,y:this.y,width:this.width,height:this.height/2}
+	this.bbd = {col:'blue',x:this.x,y:this.y+(this.height/2),width:this.width, height:this.height/2}
+	this.bbl = {col:'orange',x:this.x-3,y:this.y+10,width:this.width/2-3,height:this.height-20 }
+	this.bbr = {col:'brown',x:this.x+(this.width/2),y:this.y+10,width:this.width/2+3,height:this.height-20}
+
+	//booleans to check before moving the character
+	this.canMoveUp = false;
+	this.canMoveDown = false;
+	this.canMoveRight = false;
+	this.canMoveLeft = false;
+	this.action = 'default';
+
+	this.jump = function(dt){
+		var collisionObjects = game.levelMap.collisionObjects;
+		for(i=0;i<collisionObjects.length;i++){
+			if(checkCollision(this,collisionObjects[i])&&this.canJump){
+				this.velocityY=-7;
+				this.canJump = false;
+			}
+		}
+	}
+
+	this.update = function(dt){
+
+		var collisionObjects = game.levelMap.collisionObjects;
+		this.velocityX *= this.friction * dt;
+
+		for(i=0;i<collisionObjects.length;i++){
+
+			objectChecking = collisionObjects[i];
+			//check for collisions above
+
+			if(checkCollision(this.bbu,objectChecking)&&objectChecking.solid){
+				this.canMoveUp = false;
+				// this.y=objectChecking.y+objectChecking.height;
+				// this.imgY=objectChecking.y+objectChecking.height;
+				break;
+			}else{
+				this.canMoveUp = true;
+			}
+		}
+
+		for(i=0;i<collisionObjects.length;i++){
+			objectChecking = collisionObjects[i];
+			//check for collisions below
+
+			if(checkCollision(this.bbd,objectChecking)&&objectChecking.solid){
+				this.canMoveDown = false;
+				//this.y=objectChecking.y-this.height+1;
+				//this.imgY=objectChecking.y-this.height+1;
+				break;
+			}else if(checkCollision(this.bbd,objectChecking)&&objectChecking.platform&&this.bbd.y-this.velocityY*dt<objectChecking.y){
+				this.canMoveDown = false;
+				break;
+			}else{
+				this.canMoveDown = true;
+			}
+		}
+
+		for(i=0;i<collisionObjects.length;i++){
+			objectChecking = collisionObjects[i];
+			if(checkCollision(this.bbl,objectChecking)&&objectChecking.solid){
+				this.canMoveLeft = false;
+				break;
+			}else{
+				this.canMoveLeft = true;
+			}
+		}
+		for(i=0;i<collisionObjects.length;i++){
+			objectChecking = collisionObjects[i];
+			if(checkCollision(this.bbr,objectChecking)&&objectChecking.solid){
+				this.canMoveRight = false;
+				break;
+			}else{
+				this.canMoveRight = true;
+			}
+		}
+		if(this.canMoveLeft){
+			this.velocityX-=this.acceleration*dt;
+		}else if(this.velocityX<0){
+			this.velocityX=0;
+		}
+
+		if(this.canMoveRight){
+			this.velocityX+=this.acceleration*dt;
+		}else if(this.velocityX>0){
+			this.velocityX=0;
+		}
+
+		if(this.canMoveDown){
+			this.velocityY += this.gravity * dt;
+
+		}else if(!this.canMoveDown&&this.velocityY>0){
+			this.velocityY = 0;
+		}
+
+		if(!this.canMoveUp&&this.velocityY<0){
+			this.velocityY=0;
+		}
+
+		this.x += this.velocityX * dt;
+		this.imgX += this.velocityX * dt;
+		this.y += this.velocityY * dt;
+		this.imgY += this.velocityY * dt;
+
+		this.bbu = {col:'yellow',x:this.x,y:this.y+this.velocityY*dt,width:this.width,height:this.height/4}
+		this.bbd = {col:'blue',x:this.x,y:this.y+(this.height-2)+this.velocityY*dt,width:this.width, height:2}
+		this.bbl = {col:'orange',x:this.x+this.velocityX*dt-1,y:this.y+3,width:this.width/2-3,height:this.height-6 }
+		this.bbr = {col:'brown',x:this.x+(this.width/2-1)+1+this.velocityX*dt,y:this.y+3,width:this.width/2+3,height:this.height-6}
+	}
+}
--- levelv02.js
+++ levelv02.js
@@ -1,60 +1,89 @@
 
+function tile(x,y,w,h,solid,platform,img,sx,sy){
+	
+	//position and size
+	this.x = x;
+	this.y = y;
+	this.width = w;
+	this.height = h;
+	
+	//image values
+	this.imgX=x;
+	this.imgY=y;
+	this.imgWidth = 64;
+	this.imgHeight = 64;
+	this.col='green';
+	this.img = img;
+	this.imgSrcX = sx;
+	this.imgSrcY = sy;
+
+	//type of tile
+	this.solid = solid;
+	this.platform = platform;
+
+}
+
 function level(level){
+
 	this.tileW = 64;
 	this.tileH = 64;
+
 	this.collisionObjects = [];
+	
 	this.map = mapGrids[level];
+
+	//iterate through the array map and generate tiles
 	this.populateMap = function(){
-		for(x=0;x<this.map.length;x++){
-			for(y=0;y<this.map[x].length;y++){
-				if(this.map[x][y]===1){
+		for(x=0;x<this.map[0].length;x++){
+			for(y=0;y<this.map.length;y++){
+				if(this.map[y][x]===1){
 					var t = new tile(x*this.tileW,y*this.tileH,this.tileW,this.tileH,true,false,game.tiles,0,0);
-					this.map[x][y]=t;
+					this.map[y][x]=t;
 					this.collisionObjects.push(t);
 				}
-				else if(this.map[x][y]===2){
+				else if(this.map[y][x]===2){
 					var t = new tile(x*this.tileW,y*this.tileH,this.tileW,this.tileH,true,false,game.tiles,this.tileW,0);
-					this.map[x][y]=t;
+					this.map[y][x]=t;
 					this.collisionObjects.push(t);
 				}
-				else if(this.map[x][y]===3){
+				else if(this.map[y][x]===3){
 					var t = new tile(x*this.tileW,y*this.tileH,this.tileW,this.tileH,true,false,game.tiles,this.tileW*2,0);
-					this.map[x][y]=t;
+					this.map[y][x]=t;
 					this.collisionObjects.push(t);
 				}
-				else if(this.map[x][y]===4){
+				else if(this.map[y][x]===4){
 					var t = new tile(x*this.tileW,y*this.tileH,this.tileW,this.tileH,true,false,game.tiles,0,this.tileH);
-					this.map[x][y]=t;
+					this.map[y][x]=t;
 					this.collisionObjects.push(t);
 				}
-				else if(this.map[x][y]===5){
+				else if(this.map[y][x]===5){
 					var t = new tile(x*this.tileW,y*this.tileH,this.tileW,this.tileH,true,false,game.tiles,this.tileW,this.tileH);
-					this.map[x][y]=t;
+					this.map[y][x]=t;
 					this.collisionObjects.push(t);
 				}
-				else if(this.map[x][y]===6){
+				else if(this.map[y][x]===6){
 					var t = new tile(x*this.tileW,y*this.tileH,this.tileW,this.tileH,true,false,game.tiles,this.tileW*2,this.tileH);
-					this.map[x][y]=t;
+					this.map[y][x]=t;
 					this.collisionObjects.push(t);
 				}
-				else if(this.map[x][y]===7){
+				else if(this.map[y][x]===7){
 					var t = new tile(x*this.tileW,y*this.tileH,this.tileW,this.tileH,false,false,game.tiles,this.tileW*3,0);
-					this.map[x][y]=t;
+					this.map[y][x]=t;
 					this.collisionObjects.push(t);
 				}
-				else if(this.map[x][y]===8){
+				else if(this.map[y][x]===8){
 					var t = new tile(x*this.tileW,y*this.tileH,this.tileW,this.tileH,false,false,game.tiles,this.tileW*3,this.tileH*2);
-					this.map[x][y]=t;
+					this.map[y][x]=t;
 					this.collisionObjects.push(t);
 				}
-				else if(this.map[x][y]===9){
+				else if(this.map[y][x]===9){
 					var t = new tile(x*this.tileW,y*this.tileH,this.tileW,this.tileH,false,true,game.tiles,this.tileW*3,this.tileH);
-					this.map[x][y]=t;
+					this.map[y][x]=t;
 					this.collisionObjects.push(t);
 
 				}else{
 					var t = new tile(x*this.tileW,y*this.tileH,this.tileW,this.tileH,false);
-					this.map[x][y]=t;
+					this.map[y][x]=t;
 				}
 			}
 		}
@@ -63,44 +92,24 @@ function level(level){
 
 var mapGrids = {
 
+	//arrays representing the level layout
 	1:[
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,5,5,5],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,5,5,5],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,5,5,5],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,6,6,6],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,4,4],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,5,5],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,6,6],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,5],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,5,5],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,5,5,5],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,5,5,5],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,5,5,5],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,6,6,6],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,4,4],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,5,5],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,6,6],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,4,4],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,5,5],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,6,6],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,2,3,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,5,5,5,5,5,3,0,0,1,3,0,0,0,0,1,2,2],
+		[2,2,2,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,5,5,5,5,5,5,6,0,0,4,6,0,0,0,0,4,5,5],
+		[5,5,5,6,0,0,0,1,3,0,0,0,1,2,2,2,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,2,2,3,0,0,4,5,5,5,5,5,5,6,0,0,4,6,0,0,0,0,4,5,5],
+		[5,5,5,6,0,0,0,4,6,0,0,0,4,5,5,5,5,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,5,5,5,6,0,0,4,5,5,5,5,5,5,6,0,0,4,6,0,0,0,0,4,5,5],
+		[5,5,5,6,0,0,0,4,6,0,0,0,4,5,5,5,5,5,2,2,2,3,0,0,1,3,0,0,0,1,3,0,0,4,5,5,5,6,0,0,4,5,5,5,5,5,5,6,0,0,4,6,0,0,0,0,4,5,5]
 	],
 	2:[],
 	3:[],
